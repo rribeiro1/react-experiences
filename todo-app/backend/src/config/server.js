f011b0ef424d425d663626325edfd492e4c277cf
Rafael Ribeiro <rafael.pr10@gmail.com>
2018-01-08 19:29:58 -0200

    Frontend Todo App React

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -3,11 +3,13 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const morgan = require('morgan')
 const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(morgan('dev'))
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}`)
---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Teste</p>
+        <h2>Missão e Visão</h2>
+        <p>Sem visão e missão</p>
+    </div>
+)
